44e5dd70b4b9d7227a5165d1f0862e06d89cd278
Noah Garcia <153493798+twirl-pound@users.noreply.github.com>
2023-12-13 13:57:48 +0700

    Create Student.h

---
+++ Student.h
@@ -0,0 +1,17 @@
+// Student.h
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+
+class Student {
+public:
+    int studentId;
+    std::string name;
+    int age;
+
+    void displayInfo();
+};
+
+#endif
